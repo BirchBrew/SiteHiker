f00a293d52411cbaff08fa4285902dd50090d679
Jake Bray <jake.d.bray@gmail.com>
2018-02-19 17:39:15 -0500

    might as well run it anyway :(

--- lib/awis.ex
+++ lib/awis.ex
@@ -16,6 +16,7 @@ defmodule AWIS do
     OwnedDomains
     LinksInCount
     SiteData
+    RelatedLinks
   ) |> Enum.join(",")
 
   ##############
--- scipts/fetch_data.exs
+++ scipts/fetch_data.exs
@@ -1,5 +1,5 @@
-url = System.argv() |> hd()
+urls = File.read!("data/top_sites_us.txt") |> String.trim() |> String.split(~r{(\r\n|\r|\n)})
 
-IO.puts("Fetching data about #{url}...")
-AWIS.get_data_for_urls([url])
+IO.puts("Fetching data about #{inspect urls}...")
+AWIS.get_data_for_urls(urls)
 IO.puts("Done!")
