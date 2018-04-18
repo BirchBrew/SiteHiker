82a9972baf20a8cff2bfb90905c5d588858d59c9
SylvanSign <jake.d.bray@gmail.com>
2018-04-17 23:23:15 -0400

    minor fixups (#86)
    * minor fixups
    
    * also add some default urls to choose from

--- lib/router/static.ex
+++ lib/router/static.ex
@@ -3,13 +3,6 @@ defmodule Router.Static do
 
   plug(:redirect_root_to_index)
 
-  # TEMP, this seems to fix my caching issue during development
-  plug(
-    Plug.Static,
-    at: "/",
-    from: "./priv/static/"
-  )
-
   plug(
     Plug.Static,
     at: "/",
